6fe6a36a2eb2c4deffb1faecb517448bc6953b8b
chunkmd <chunkmd@gmail.com>
2022-12-04 22:16:36 +0200

    acum merge

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,70 +1,114 @@
-import styles from '../styles/Galery.module.css'
-import Image from 'next/image';
-import Zoom from 'react-medium-image-zoom'
-import 'react-medium-image-zoom/dist/styles.css'
-import { motion } from 'framer-motion';
-import Head from 'next/head'
-import img1 from '../public/gallery_images/24.jpeg'
-import img2 from '../public/gallery_images/16.jpg'
-import img3 from '../public/gallery_images/22.jpeg'
-import img4 from '../public/gallery_images/50.jpg'
-import img5 from '../public/gallery_images/51.jpg'
-import img6 from '../public/gallery_images/52.jpg'
-import img7 from '../public/gallery_images/53.jpg'
-import img8 from '../public/gallery_images/1.jpg'
-import img9 from '../public/gallery_images/18.jpg'
-import img10 from '../public/gallery_images/14.jpg'
-import img11 from '../public/gallery_images/11.jpg'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import styles from "../styles/Galery.module.css";
+import Image from "next/image";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+import { motion } from "framer-motion";
+import Head from "next/head";
+import { useState } from "react";
+
+import img8 from "../public/gallery_images/1.jpg";
+
+import { db } from "../utils/firebase";
+
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
 
 import "swiper/css";
 import "swiper/css/navigation";
 
-const Gallery = () => {
-  return ( 
-<>
+//-------Get Props--------------------------------
+export const getServerSideProps = async () => {
+  const images = await db.collection("photo").limit(20).get();
 
-<Head>
-  <title> YourMobila | Proiecte </title>
-   <meta name='keywords' content='Proiecte mobila fotografii Brașov' />
-   <link rel="icon" href="/favicon.ico" />
-   <meta name='description' content='Galereie cu proiectele realizate' /> 
-</Head>
+  const imagesData = images.docs.map((img) => ({
+    id: img.id,
+    ...img.data(),
+    params: { id: img.id.toString() },
+  }));
 
+  console.log("Images \n", imagesData);
 
-<h1 className={styles.heading}>Bucătărie</h1>
-<Swiper
-  spaceBetween={24}
-  slidesPerView={5}
-  navigation={true} 
-  modules={[Navigation]}
-  >
-<SwiperSlide>
-    <Zoom wrapStyle={{ maxWidth: "100%", minHeight: "100%" }} >
-<Image src={img1} />
-</Zoom>
-</SwiperSlide>    
-</Swiper>
+  return {
+    props: {
+      images: JSON.parse(JSON.stringify(imagesData)),
+    },
+  };
+};
 
-<h1 className={styles.heading}>Baie</h1>
-<Swiper
-  spaceBetween={24}
-  slidesPerView={5}
-  navigation={true} 
-  modules={[Navigation]}
-  >
-<SwiperSlide>
-    <Zoom wrapStyle={{ maxWidth: "100%", minHeight: "100%" }} >
-<Image src={img1} />
-</Zoom>
-</SwiperSlide>    
-</Swiper>
+//----------------------------------------------------
 
+const Gallery = (props) => {
+  const [images, setImages] = useState(props.images);
+  const getImagesByTag = async () => {
+    setImages([]);
+    //--------------------
+    //GET THE IMAGES HERE
+    //--------------------
+    setImages(images);
+  };
+  return (
+    <>
+      <Head>
+        <title> YourMobila | Proiecte </title>
+        <meta name="keywords" content="Proiecte mobila fotografii Brașov" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Galereie cu proiectele realizate" />
+      </Head>
 
-</>
+      <h1 className={styles.heading}>Bucătărie</h1>
 
-   );
-}
- 
-export default Gallery
+      <Swiper
+        spaceBetween={24}
+        slidesPerView={5}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {/* //map Bucatarie images */}
+
+        {images.map((img) => {
+          if(img.tag == "Bucatarie")
+          return (
+            <SwiperSlide key={img.id}>
+              <Zoom wrapStyle={{ maxWidth: "100%", minHeight: "100%" }}>
+                <Image
+                  src={img.imagine[0].downloadURL}
+                  alt="product image"
+                  layout="responsive"
+                  width={"100%"}
+                  height={"100%"}
+                />
+              </Zoom>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+
+      <h1 className={styles.heading}>Baie</h1>
+      <Swiper
+        spaceBetween={24}
+        slidesPerView={5}
+        navigation={true}
+        modules={[Navigation]}
+      >
+        {images.map((img) => {
+          if(img.tag == "Baie")
+          return (
+            <SwiperSlide key={img.id}>
+              <Zoom wrapStyle={{ maxWidth: "100%", minHeight: "100%" }}>
+                <Image
+                  src={img.imagine[0].downloadURL}
+                  alt="product image"
+                  layout="responsive"
+                  width={"100%"}
+                  height={"100%"}
+                />
+              </Zoom>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
+};
+
+export default Gallery;
---
+++ utils/firebase.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/compat/app";
+import next from "next";
+
+import "firebase/compat/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+export { db };
+
+
+
+// process.env.NEXT_PUBLIC_FIREBASE_API_KEY
+// process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+// process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+// process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+// process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+// process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
